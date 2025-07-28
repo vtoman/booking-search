73f439ff23ad47aac9b53247521f47a883b0a43e
Viktor Toman <vtoman@gmail.com>
2025-07-28 10:03:00 +0200

    Adding several more locations.

--- locations.js
+++ locations.js
@@ -1,8 +1,11 @@
 // locations.js
 
 module.exports = [
-  "Olympic Beach",
+  "Olympic Beach, Macedonia, Greece",
   "Leptokaria, Macedonia, Greece",
   "Neos Marmaras, Macedonia, Greece",
   "Sarti, Macedonia, Greece",
+  "Perea, Macedonia, Greece",
+  "Vergia, Macedonia, Greece",
+  "Paralia Katerinis, Macedonia, Greece",
 ];
